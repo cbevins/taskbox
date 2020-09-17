457a3f0faa37e04f02ed28a940cf9dfc8b0d05df
Collin D. Bevins <cbevins@montana.com>
2020-09-17 13:28:47 -0600

    Created BehaviorSelector.svelte

---
+++ src/components/svelte/firescope/input/BehaviorSelector.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let _input
+  let behaviors = [
+    { v: 'spreadRate', text: 'Spread Rate'},
+    { v: 'flameLength', text: 'Flame Length'},
+    { v: 'scorchHeight', text: 'ScorchHeight'},
+  ]
+</script>
+
+<select class='select-css' bind:value={$_input.behavior}>
+	{#each behaviors as b}
+		<option value={b.v}>
+			{b.text}
+		</option>
+	{/each}
+</select>

22b04e4fe0a47c1a7c3359e5dd1bdbbd907806ef
Collin D. Bevins <cbevins@montana.com>
2020-09-19 16:22:18 -0600

    Added tabbed page navigation

--- src/components/svelte/firescope/input/Panel.svelte
+++ src/components/svelte/firescope/input/Panel.svelte
@@ -1,5 +1,6 @@
 <script>
-  import { Container, Row, Col, Table } from 'sveltestrap'
+	import { fade, slide } from 'svelte/transition';
+  import { Button, Collapse, Container, Row, Col, Table } from 'sveltestrap'
 
   import BehaviorSelector from './BehaviorSelector.svelte'
   import FirescopeAirTemperature from '../sliders/FirescopeAirTemperature.svelte'
@@ -10,27 +11,71 @@
   import FirescopeWindSpeedDirection from '../sliders/FirescopeWindSpeedDirection.svelte'
   import FirescopeReels from '../output/Reels.svelte'
   import FireScope from '../scope/Scope.svelte'
+  import FireTable from '../tables/FireTable.svelte'
   import FuelSelector from './FuelSelector.svelte'
   import UnitsSelector from './UnitsSelector.svelte'
 
   export let _input
   export let _output
+  let activePage = 'firescope'
+  let showInputs = true
+  let showReels = false
+  function handleInputs() {showInputs = !showInputs}
+  function handleReels() {showReels = !showReels}
+  function showPage(e,p) {activePage=p}
 </script>
 
+<svelte:head>
+  <link rel="stylesheet" href="bootstrap.min.css">
+</svelte:head>
 
 <Container>
   <Row>
+    <Button outline color="primary" size='sm' class="mb-3"
+      on:click={e => showPage(e,'firescope')}>
+      FireScope
+    </Button>
+    <Button outline color="primary" size='sm' class="mb-3"
+      on:click={e => showPage(e,'tables')}>
+      Tables
+    </Button>
+    <Button outline color="primary" size='sm' class="mb-3"
+        on:click={e => showPage(e,'graphs')}>
+      Graphs
+    </Button>
+    <Button outline color="secondary" size='sm' class="mb-3"
+        on:click={handleReels}>
+      Toggle Reels
+    </Button>
+    <Button outline color="secondary" size='sm' class="mb-3"
+        on:click={handleInputs}>
+      Toggle Inputs
+    </Button>
+  </Row>
+  <Row>
     <Col xs='12'>
-      <FireScope width=300 height=300 _output={_output} _input={_input}/>
+      {#if activePage === 'firescope'}
+        <FireScope width=300 height=300 _output={_output} _input={_input}/>
+      {:else if activePage === 'graphs'}
+        <p class='dummy-page' transition:fade>This will be the Graphs Page!</p>
+      {:else if activePage === 'tables'}
+        <FireTable _output={_output} _input={_input}/>
+      {/if}
     </Col>
   </Row>
   <Row>
     <Col>
-      <FirescopeReels _output={_output} _input={_input}/>
+      {#if showReels}
+      <div transition:fade>
+        <FirescopeReels _output={_output} _input={_input} />
+      </div>
+      {/if}
     </Col>
   </Row>
+
   <Row>
-<Table>
+{#if showInputs}
+<table>
   <tr>
     <td colspan='3'><FuelSelector _output={_output} _input={_input}/></td>
     <td><UnitsSelector _input={_input} _output={_output} /></td>
@@ -44,6 +89,14 @@
     <td><FirescopeAirTemperature _input={_input} _output={_output} /></td>
     <td><FirescopeElapsedTime _input={_input} _output={_output} /></td>
   </tr>
-</Table>
+</table>
+{/if}
 </Row>
 </Container>
+
+<style>
+  .dummy-page {
+    height: 300px;
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.48);
+  }
+</style>

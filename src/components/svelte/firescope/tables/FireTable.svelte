1a915805d480c7e948aa75f8f6d44f812fec4cf2
Collin D. Bevins <cbevins@montana.com>
2020-09-19 16:34:30 -0600

    Added more output rows

--- src/components/svelte/firescope/tables/FireTable.svelte
+++ src/components/svelte/firescope/tables/FireTable.svelte
@@ -3,27 +3,34 @@
   export let _input
   export let _output
   let uom = $_input.uom
+$: {
+  uom = $_input.uom
+}
+  const Items = [
+    {label: 'Fire Area', prop: 'fireArea', dec: 0},
+    {label: 'Fire Perimeter', prop: 'firePerimeter', dec: 0},
+    {label: 'Fire Heading from North', prop: 'fireHeadingFromNorth', dec: 0},
+    {label: 'Heading Spread Rate', prop: 'headingSpreadRate', dec: 2},
+    {label: 'Heading Spread Distance', prop: 'headingSpreadDistance', dec: 0},
+    {label: 'Heading Flame Length', prop: 'headingFlameLength', dec: 2},
+    {label: 'Heading Scorch Height', prop: 'headingScorchHeight', dec: 0},
+    {label: 'Flanking Spread Rate', prop: 'flankingSpreadRate', dec: 2},
+    {label: 'Flanking Spread Distance', prop: 'flankingSpreadDistance', dec: 0},
+    {label: 'Flanking Flame Length', prop: 'flankingFlameLength', dec: 2},
+    {label: 'Flanking Scorch Height', prop: 'flankingScorchHeight', dec: 0},
+    {label: 'Backing Spread Rate', prop: 'backingSpreadRate', dec: 2},
+    {label: 'Backing Spread Distance', prop: 'backingSpreadDistance', dec: 0},
+    {label: 'Backing Flame Length', prop: 'backingFlameLength', dec: 2},
+    {label: 'Backing Scorch Height', prop: 'backingScorchHeight', dec: 0},
+  ]
 </script>
 
 <Table>
-  <tr>
-    <td>Heading Spread Rate</td>
-    <td>{$_output.headingSpreadRate.v[uom].toFixed(2)}</td>
-    <td>{$_output.headingSpreadRate.u[uom]}</td>
-  </tr>
-  <tr>
-    <td>Heading Spread Distance</td>
-    <td>{$_output.headingSpreadDistance.v[uom].toFixed(2)}</td>
-    <td>{$_output.headingSpreadDistance.u[uom]}</td>
-  </tr>
-  <tr>
-    <td>Heading Flame Length</td>
-    <td>{$_output.headingFlameLength.v[uom].toFixed(2)}</td>
-    <td>{$_output.headingFlameLength.u[uom]}</td>
-  </tr>
-  <tr>
-    <td>Heading Scorch Height</td>
-    <td>{$_output.headingScorchHeight.v[uom].toFixed(2)}</td>
-    <td>{$_output.headingScorchHeight.u[uom]}</td>
-  </tr>
+  {#each Items as item}
+    <tr>
+      <td>{item.label}</td>
+      <td align='right'>{$_output[item.prop].v[uom].toFixed(item.dec)}</td>
+      <td align='left'>{$_output[item.prop].u[uom]}</td>
+    </tr>
+  {/each}
 </Table>

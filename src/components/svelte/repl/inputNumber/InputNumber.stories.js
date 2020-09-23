69085f23d6f9178865e2c508207e2a0cee6d7adf
Collin D. Bevins <cbevins@montana.com>
2020-09-23 10:41:38 -0600

    Added InputNumber tutorial example

--- src/components/svelte/repl/inputNumber/InputNumber.stories.js
+++ src/components/svelte/repl/inputNumber/InputNumber.stories.js
@@ -1,18 +1,12 @@
 import { withKnobs, object } from '@storybook/addon-knobs'
-import InputNumber from '../inputNumber/InputNumber.svelte';
-import Tree from './Tree.svelte';
+import InputNumber from './InputNumber.svelte';
 
 export default {
-  title: 'Svelte/REPL/TreeView',
+  title: 'Svelte/REPL/InputNumber',
   Component: Tree,
 };
 
 export const tutorialDemo = () => ({
-  Component: Tree,
-  props: {},
-});
-
-export const inputNumberDemo = () => ({
   Component: InputNumber,
   props: {
     a: object('a', 4),

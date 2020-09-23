44718a5747f2ec21cb0b2758029bb2ecc0359f2c
Collin D. Bevins <cbevins@montana.com>
2020-09-23 10:35:12 -0600

    Use compass.js and firescope.cs

--- src/components/svelte/firescope/sliders/SlopeAspect.svelte
+++ src/components/svelte/firescope/sliders/SlopeAspect.svelte
@@ -1,61 +1,36 @@
 <script>
+  import * as Compass from './compass.js'
   export let aspect
   export let percent
 
-  export let degrees = slopeDegrees(0.01 * percent)
-  let direction = compassDirection(aspect)
-
-  function toDegrees (rad) {
-    return (rad * 180) / Math.PI
-  }
-  function toRadians (deg) {
-    return (deg * Math.PI) / 180
-  }
-  function slopeDegrees (ratio) {
-    const rad = Math.atan(ratio)
-    return toDegrees(rad)
-  }
-  function slopeRatio (deg) {
-    const rad = toRadians(deg)
-    return Math.tan(rad)
-  }
-  function compassDirection(azimuth) {
-    const Dir = ['NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE',
-      'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW']
-    const span = 360 / (Dir.length + 1)
-    const half = span / 2
-    if (azimuth < half || azimuth >= 360-half) return 'N'
-    const idx = Math.trunc((azimuth - half) / span)
-    return Dir[idx]
-  }
-  function handleCompass() {
-    direction = compassDirection(aspect)
-  }
-  function handleSteepness() {
-    percent = 100 * slopeRatio(degrees)
-  }
+  export let degrees = Compass.slopeDegrees(0.01 * percent)
+  let direction = Compass.point(aspect)
+  function handleCompass() {direction = Compass.point(aspect)}
+  function handleSteepness() {percent = 100 * Compass.slopeRatio(degrees)}
 </script>
 
-<div class="control-wrapper">
-  <div class="slider-row">
-      <p class="slope-title">Slope</p>
-  </div>
-  <div class="slider-row">
-    <div class="slope-slider">
-      <p class="slope-label">Steep</p>
-      <p class="slope-value">{degrees.toFixed(0)}&#x00B0;</p>
-      <p class="slope-value">{percent.toFixed(0)}%</p>
-      <div class="vertical-slider-wrapper">
+<svelte:head>
+  <link rel="stylesheet" href="firescope.css">
+</svelte:head>
+
+<div class="vslider-group">
+  <p class="firescope-vslider-title">Slope</p>
+  <div>
+    <div class="firescope-vslider">
+      <p class="firescope-vslider-label">Steep</p>
+      <p class="firescope-vslider-value">{degrees.toFixed(0)}&#x00B0;</p>
+      <p class="firescope-vslider-value">{percent.toFixed(0)}%</p>
+      <div class="firescope-vslider-wrapper">
         <input bind:value={degrees} type="range" min="0" max="80" step="1"
         on:input={handleSteepness}>
       </div>
     </div>
 
-    <div class="slope-slider">
-      <p class="slope-label">Asp</p>
-      <p class="slope-value">{aspect}&#x00B0;</p>
-      <p class="slope-value">{direction}</p>
-      <div class="vertical-slider-wrapper">
+    <div class="firescope-vslider">
+      <p class="firescope-vslider-label">Asp</p>
+      <p class="firescope-vslider-value">{aspect}&#x00B0;</p>
+      <p class="firescope-vslider-value">{direction}</p>
+      <div class="firescope-vslider-wrapper">
         <input bind:value={aspect} type="range" min="0" max="359" step="1"
         on:input={handleCompass}>
       </div>
@@ -64,66 +39,10 @@
 </div>
 
 <style>
-* {
-  box-sizing: border-box;
-}
-.slider-row::after {
-  content: "";
-  clear: both;
-  display: table;
-}
-.control-wrapper {
-  width: 80px;
+.vslider-group {
+  width: 90px;
   height: 220px;
   text-align: center;
   box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,255,0,0.48);
-}
-.slope-label {
-  font-family: "Lucida Sans", sans-serif;
-  font-size: 12px;
-  text-align: center;
-  color: green;
-  margin: 0px;
-  line-height: 1.2;
-}
-.slope-title {
-  font-family: "Lucida Sans", sans-serif;
-  font-size: 12px;
-  text-align: center;
-  color: green;
-  margin: 0px;
-  line-height: 1.5;
-}
-.slope-value {
-  font-family: "Lucida Sans", sans-serif;
-  font-size: 12px;
-  text-align: center;
-  color: green;
-  margin: 0px;
-  line-height: 1.2;
-}
-.slope-slider {
-  font-family: "Lucida Sans", sans-serif;
-  font-size: 12px;
-  width: 40px;
-  height: 220px;
-  padding-left: 5px;
-  padding-right: 5px;
-  margin: 0;
-  float: left;
-}
-.vertical-slider-wrapper {
-  display: inline-block;
-  width: 20px;
-  height: 150px;
-  padding: 0;
-  box-shadow: 0 1px 3px rgba(0,0,0,0.24), 0 1px 2px rgba(0,0,0,0.48);
-}
-.vertical-slider-wrapper input {
-  width: 150px;
-  height: 20px;
-  margin: 0;
-  transform-origin: 75px 75px;
-  transform: rotate(-90deg);
 }
 </style>
--- src/components/svelte/firescope/sliders/WindSpeedDirection.svelte
+++ src/components/svelte/firescope/sliders/WindSpeedDirection.svelte
@@ -1,60 +1,36 @@
 <script>
+  import * as Compass from './compass.js'
   export let from
   export let speed
-
-  let direction = compassDirection(from)
-
-  function toDegrees (rad) {
-    return (rad * 180) / Math.PI
-  }
-  function toRadians (deg) {
-    return (deg * Math.PI) / 180
-  }
-  function slopeDegrees (ratio) {
-    const rad = Math.atan(ratio)
-    return toDegrees(rad)
-  }
-  function slopeRatio (deg) {
-    const rad = toRadians(deg)
-    return Math.tan(rad)
-  }
-  function compassDirection(azimuth) {
-    const Dir = ['NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE',
-      'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW']
-    const span = 360 / (Dir.length + 1)
-    const half = span / 2
-    if (azimuth < half || azimuth >= 360-half) return 'N'
-    const idx = Math.trunc((azimuth - half) / span)
-    return Dir[idx]
-  }
-  function handleCompass() {
-    direction = compassDirection(from)
-  }
+  let direction = Compass.point(from)
+  function handleCompass() {direction = Compass.point(from)}
 </script>
 
-<div class="control-wrapper">
-  <div class="slider-row">
-      <p class="wind-title">Wind at</p>
-  </div>
-  <div class="slider-row">
-    <div class="wind-slider">
-      <p class="wind-label">Midflm</p>
-      <p class="wind-value">{speed.toFixed(0)}
-        <span class='units-text'>mph</span>
+<svelte:head>
+  <link rel="stylesheet" href="firescope.css">
+</svelte:head>
+
+<div class="vslider-group">
+  <p class="firescope-vslider-title">Wind at</p>
+  <div>
+    <div class="firescope-vslider">
+      <p class="firescope-vslider-label">Midflm</p>
+      <p class="firescope-vslider-value">{speed.toFixed(0)}
+        <span class='firescope-vslider-units'>mph</span>
       </p>
-      <p class="wind-value">{(1.60934*speed).toFixed(0)}
-        <span class='units-text'>kph</span>
+      <p class="firescope-vslider-value">{(1.60934*speed).toFixed(0)}
+        <span class='firescope-vslider-units'>kph</span>
       </p>
-      <div class="vertical-slider-wrapper">
+      <div class="firescope-vslider-wrapper">
         <input bind:value={speed} type="range" min="0" max="40" step="1" />
       </div>
     </div>
 
-    <div class="wind-slider">
-      <p class="wind-label">From</p>
-      <p class="wind-value">{from}&#x00B0;</p>
-      <p class="wind-value">{direction}</p>
-      <div class="vertical-slider-wrapper">
+    <div class="firescope-vslider">
+      <p class="firescope-vslider-label">From</p>
+      <p class="firescope-vslider-value">{from}&#x00B0;</p>
+      <p class="firescope-vslider-value">{direction}</p>
+      <div class="firescope-vslider-wrapper">
         <input bind:value={from} type="range" min="0" max="359" step="1"
           on:input={handleCompass}>
       </div>
@@ -63,72 +39,10 @@
 </div>
 
 <style>
-* {
-  box-sizing: border-box;
-}
-.slider-row::after {
-  content: "";
-  clear: both;
-  display: table;
-}
-.control-wrapper {
+.vslider-group {
   width: 90px;
   height: 220px;
   text-align: center;
   box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,255,0,0.48);
-}
-
-.units-text {
-  font-family: "Lucida Sans", sans-serif;
-  font-size: 6px;
-  text-align: left;
-  color: green;
-  padding: 0;
-  margin: 0px;
-}
-.wind-label {
-  font-family: "Lucida Sans", sans-serif;
-  font-size: 12px;
-  text-align: center;
-  color: green;
-  margin: 0px;
-}
-.wind-title {
-  font-family: "Lucida Sans", sans-serif;
-  font-size: 12px;
-  text-align: center;
-  color: green;
-  margin: 0px;
-}
-.wind-value {
-  font-family: "Lucida Sans", sans-serif;
-  font-size: 12px;
-  text-align: center;
-  color: green;
-  margin: 0px;
-}
-.wind-slider {
-  font-family: "Lucida Sans", sans-serif;
-  font-size: 12px;
-  width: 45px;
-  height: 220px;
-  padding-left: 5px;
-  padding-right: 5px;
-  margin: 0;
-  float: left;
-}
-.vertical-slider-wrapper {
-  display: inline-block;
-  width: 20px;
-  height: 150px;
-  padding: 0;
-  box-shadow: 0 1px 3px rgba(0,0,0,0.24), 0 1px 2px rgba(0,0,0,0.48);
-}
-.vertical-slider-wrapper input {
-  width: 150px;
-  height: 20px;
-  margin: 0;
-  transform-origin: 75px 75px;
-  transform: rotate(-90deg);
 }
 </style>
